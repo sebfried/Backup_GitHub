66b215b20f0b3cc5ab0277a8a5d941e376671506
sebfried <dev@sebastianfriedrich.at>
2024-11-21 17:40:25 +0100

    v1.72

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##################################################################
-# GitHub Backup Script v1.71
+# GitHub Backup Script v1.72
 ##################################################################
 
 # Get the directory where the script is located
